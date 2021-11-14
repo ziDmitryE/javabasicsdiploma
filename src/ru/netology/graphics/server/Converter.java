a1836ad6aa0459f724417f15e025a9a65aec2c62
Dmitriy Eremenko <er_inbox@mail.ru>
2021-11-14 16:47:50 +0300

    Проверка инкапсуляции, форматирование кода, камелкэйз

--- src/ru/netology/graphics/server/Converter.java
+++ src/ru/netology/graphics/server/Converter.java
@@ -14,36 +14,40 @@ import java.net.URL;
 
 public class Converter implements TextGraphicsConverter {
 
-    public double maxRatio;
-    public int maxWidth;
-    public int maxHeight;
-    public TextColorSchema newSchema;
+    private double maxRatio;
+    private int maxWidth;
+    private int maxHeight;
+    private TextColorSchema newSchema;
 
     @Override
     public String convert(String url) throws IOException, BadImageSizeException {
         BufferedImage img = ImageIO.read(new URL(url));
-        if(maxRatio!=0 & img.getWidth()/img.getHeight() > maxRatio){
-            throw new BadImageSizeException(img.getWidth()/img.getHeight(),maxRatio);
+        if (maxRatio != 0 & img.getWidth() / img.getHeight() > maxRatio) {
+            throw new BadImageSizeException(img.getWidth() / img.getHeight(), maxRatio);
         }
         int K1;
-        if(maxWidth!=0 & maxWidth<img.getWidth()){
-            K1 = img.getWidth()/maxWidth;
-        } else { K1 = 0; }
+        if (maxWidth != 0 & maxWidth < img.getWidth()) {
+            K1 = img.getWidth() / maxWidth;
+        } else {
+            K1 = 0;
+        }
         int K2;
-        if(maxHeight!=0 & maxHeight<img.getHeight()){
-            K2 = img.getHeight()/maxHeight;
-        } else { K2 = 0; }
+        if (maxHeight != 0 & maxHeight < img.getHeight()) {
+            K2 = img.getHeight() / maxHeight;
+        } else {
+            K2 = 0;
+        }
         int newWidth;
         int newHeight;
-        if(K1!=0 | K2!=0){
-            if(K1>=K2){
-                newWidth = img.getWidth()/K1;
-                newHeight = img.getHeight()/K1;
-            }else {
-                newWidth = img.getWidth()/K2;
-                newHeight = img.getHeight()/K2;
+        if (K1 != 0 | K2 != 0) {
+            if (K1 >= K2) {
+                newWidth = img.getWidth() / K1;
+                newHeight = img.getHeight() / K1;
+            } else {
+                newWidth = img.getWidth() / K2;
+                newHeight = img.getHeight() / K2;
             }
-        }else {
+        } else {
             newWidth = img.getWidth();
             newHeight = img.getHeight();
         }
@@ -55,12 +59,12 @@ public class Converter implements TextGraphicsConverter {
         char[][] text = new char[bwImg.getHeight()][bwImg.getWidth()];
         StringBuilder sb = new StringBuilder();
         Symbols schema = new Symbols();
-        if (newSchema == null){
+        if (newSchema == null) {
             newSchema = schema;
         }
         int[] p = new int[3];
-        for (int w=0; w<bwImg.getHeight(); w++){
-            for(int h=0; h<bwImg.getWidth(); h++){
+        for (int w = 0; w < bwImg.getHeight(); w++) {
+            for (int h = 0; h < bwImg.getWidth(); h++) {
                 int color = bwRaster.getPixel(h, w, p)[0];
                 text[w][h] = newSchema.convert(color);
                 sb.append(text[w][h]).append(text[w][h]).append(text[w][h]);
